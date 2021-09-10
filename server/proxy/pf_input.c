d4355a611a3d585e098d1b3b0a0ef76dcc0b4a9c
akallabeth <akallabeth@users.noreply.github.com>
2021-09-10 10:39:07 +0200

    Added proxy input state sync (#7282)
    The proxy server component might receive input related events
    before the proxy client has established the connection to the
    target machine.
    With this change, the current keyboard state is cached and sent
    to the target when it is ready. All input events received before
    the target is ready are discarded.
    
    (cherry picked from commit 4d23bc9372fc1d101ed6ff3841ab4567fd384d5f)

--- server/proxy/pf_input.c
+++ server/proxy/pf_input.c
@@ -44,6 +44,19 @@ static BOOL pf_server_check_and_sync_input_state(pClientContext* pc)
 	return TRUE;
 }
 
+static BOOL pf_server_check_and_sync_input_state(pClientContext* pc)
+{
+	if (freerdp_get_state(&pc->context) < CONNECTION_STATE_ACTIVE)
+		return FALSE;
+	if (pc->input_state_sync_pending)
+	{
+		BOOL rc = freerdp_input_send_synchronize_event(pc->context.input, pc->input_state);
+		if (rc)
+			pc->input_state_sync_pending = FALSE;
+	}
+	return TRUE;
+}
+
 static BOOL pf_server_synchronize_event(rdpInput* input, UINT32 flags)
 {
 	pServerContext* ps = NULL;
@@ -84,6 +97,9 @@ static BOOL pf_server_keyboard_event(rdpInput* input, UINT16 flags, UINT8 code)
 	if (!pf_server_check_and_sync_input_state(pc))
 		return TRUE;
 
+	if (!pf_server_check_and_sync_input_state(pc))
+		return TRUE;
+
 	if (!config->Keyboard)
 		return TRUE;
 
@@ -113,6 +129,9 @@ static BOOL pf_server_unicode_keyboard_event(rdpInput* input, UINT16 flags, UINT
 
 	config = ps->pdata->config;
 	WINPR_ASSERT(config);
+
+	if (!pf_server_check_and_sync_input_state(pc))
+		return TRUE;
 
 	if (!pf_server_check_and_sync_input_state(pc))
 		return TRUE;
@@ -148,6 +167,9 @@ static BOOL pf_server_mouse_event(rdpInput* input, UINT16 flags, UINT16 x, UINT1
 	if (!pf_server_check_and_sync_input_state(pc))
 		return TRUE;
 
+	if (!pf_server_check_and_sync_input_state(pc))
+		return TRUE;
+
 	if (!config->Mouse)
 		return TRUE;
 
@@ -178,6 +200,9 @@ static BOOL pf_server_extended_mouse_event(rdpInput* input, UINT16 flags, UINT16
 
 	config = ps->pdata->config;
 	WINPR_ASSERT(config);
+
+	if (!pf_server_check_and_sync_input_state(pc))
+		return TRUE;
 
 	if (!pf_server_check_and_sync_input_state(pc))
 		return TRUE;
