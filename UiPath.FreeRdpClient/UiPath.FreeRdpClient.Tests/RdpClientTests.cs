b5dcac9f1dce0f0e22a5dc5198804ea70faf4f31
mihainradu <mihainradu@users.noreply.github.com>
2023-03-02 16:16:17 +0200

    revert tests

--- UiPath.FreeRdpClient/UiPath.FreeRdpClient.Tests/RdpClientTests.cs
+++ UiPath.FreeRdpClient/UiPath.FreeRdpClient.Tests/RdpClientTests.cs
@@ -47,24 +47,28 @@ public class RdpClientTests : TestsBase
     public async Task ShouldConnect(int colorDepthInput, int expectedWtsApiValue)
     {
         var user = await Host.GivenUser();
-        EnableFreeRdpLogs();
         var connectionSettings = new RdpConnectionSettings(
-            username: "User",
-            password: "somePass4U@3",
-            domain: "SESSION"
+            username: user.UserName.Split("\\")[1],
+            password: user.Password,
+            domain: user.UserName.Split("\\")[0]
         )
         {
-            HostName = "WinS2019Tests",
-            DesktopWidth = 3*4*101,
-            DesktopHeight = 3*4*71,
+            DesktopWidth = 3 * 4 * 101,
+            DesktopHeight = 3 * 4 * 71,
             ColorDepth = colorDepthInput
         };
-        Host.GetRequiredService<ILogger<TestHost>>().BeginScope("{RunId}", "runId_someAmbientRunId");
-        using var activity = new Activity("ShouldConnect").SetBaggage("RunId", $"runId_{connectionSettings.ClientName}").Start();
+
         await using var sut = await FreeRdpClient.Connect(connectionSettings);
-        await Task.Delay(50000);
+        var sessionId = WtsApi.FindFirstSessionByClientName(connectionSettings.ClientName);
+        sessionId.HasValue.ShouldBeTrue();
+        var displayInfo = WtsApi.GetSessionDisplayInfo(sessionId.Value);
+
+        ((int)displayInfo.HorizontalResolution).ShouldBe(connectionSettings.DesktopWidth);
+        ((int)displayInfo.VerticalResolution).ShouldBe(connectionSettings.DesktopHeight);
+        //((int)displayInfo.ColorDepth).ShouldBe(expectedWtsApiValue);
 
         await sut.DisposeAsync();
+        await WaitFor.Predicate(() => WtsApi.FindFirstSessionByClientName(connectionSettings.ClientName) == null);
     }
 
     private void EnableFreeRdpLogs()
