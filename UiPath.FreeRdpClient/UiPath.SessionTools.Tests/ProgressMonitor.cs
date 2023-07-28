b4ec2044ab0de2ca8328db65814a401c1312bfc9
Eduard Florin Dumitru <eduard.dumitru@uipath.com>
2023-07-28 17:21:41 +0200

    fixes(SessionTools and FreeRdp docs):
    - fix flaky time-based tests in SessionTools
    - remove broken hyperlink from the root README.md

---
+++ UiPath.FreeRdpClient/UiPath.SessionTools.Tests/ProgressMonitor.cs
@@ -0,0 +1,44 @@
+﻿using Nito.AsyncEx;
+
+namespace UiPath.SessionTools.Tests;
+
+internal sealed class ProgressMonitor<T> : IProgress<T>
+    where T : IEquatable<T>
+{
+    private readonly AsyncMonitor _asyncMonitor = new();
+    private readonly List<T> _lines = new();
+
+    public async Task WaitForCount(int count, CancellationToken ct)
+    {
+        using (await _asyncMonitor.EnterAsync(ct))
+        {
+            while (_lines.Count < count)
+            {
+                await _asyncMonitor.WaitAsync(ct);
+            }
+        }
+    }
+
+    public async Task WaitForValue(Func<T, bool> predicate, CancellationToken ct)
+    {
+        int cChecked = 0;
+
+        using (await _asyncMonitor.EnterAsync(ct))
+        {
+            while (!_lines.Skip(cChecked).Any(predicate))
+            {
+                cChecked = _lines.Count;
+                await _asyncMonitor.WaitAsync(ct);
+            }
+        }
+    }
+
+    async void IProgress<T>.Report(T value)
+    {
+        using (await _asyncMonitor.EnterAsync())
+        {
+            _lines.Add(value);
+            _asyncMonitor.PulseAll();
+        }
+    }
+}
--- UiPath.FreeRdpClient/UiPath.SessionTools/Processes/ProcessRunner.cs
+++ UiPath.FreeRdpClient/UiPath.SessionTools/Processes/ProcessRunner.cs
@@ -25,7 +25,10 @@ public partial class ProcessRunner
         _logger = logger;
     }
 
-    public virtual async Task<Report> Run(string fileName, string arguments, string workingDirectory, CancellationToken ct = default)
+    public virtual Task<Report> Run(string fileName, string arguments, string workingDirectory, CancellationToken ct = default)
+    => RunCore(fileName, arguments, workingDirectory, stdoutProgress: null, stderrProgress: null, ct: ct);
+
+    internal async Task<Report> RunCore(string fileName, string arguments, string workingDirectory, IProgress<string>? stdoutProgress = null, IProgress<string>? stderrProgress = null, CancellationToken ct = default)
     {
         var startInfo = StartInfo();
         _logger?.LogRunStarted(startInfo);
@@ -36,8 +39,8 @@ public partial class ProcessRunner
         int processId = process.Id;
         DateTime processStartTime = process.StartTime;
 
-        Pump stdout = new(process.StandardOutput);
-        Pump stderr = new(process.StandardError);
+        Pump stdout = new(process.StandardOutput, stdoutProgress);
+        Pump stderr = new(process.StandardError, stderrProgress);
 
         try
         {
