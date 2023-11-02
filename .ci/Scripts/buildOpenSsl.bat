b0ad4094393b5cf15ab4e4a0a2b9f8975d8d0bff
Eduard Florin Dumitru <eduard.dumitru@uipath.com>
2023-11-02 12:59:21 +0100

    FreeRDP updates - copied branch "uipath" and rebased onto tag "2.11.2" - bumped OpenSSL to tag "openssl-3.0.12" - fixed breaks: - updated linker deps in FreeRdpWrapper for both Debug and Release

--- .ci/Scripts/buildOpenSsl.bat
+++ .ci/Scripts/buildOpenSsl.bat
@@ -6,7 +6,6 @@ pushd .
 call "%vsDir%\VC\Auxiliary\Build\vcvars64.bat"
 popd
 
-perl Configure VC-WIN64A no-asm --prefix=..\OpenSSL-VC-64
-call ms\do_win64a
-nmake -f ms\nt.mak
-nmake -f ms\nt.mak install
+perl Configure VC-WIN64A no-asm no-shared --prefix=%~dp0\..\..\..\OpenSSL-VC-64
+nmake
+nmake install
