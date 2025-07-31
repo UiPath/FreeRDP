54d718fe4b28e7fc20d3e0cf68b54ea8cd5e9d39
Cristi <cristian.dima@uipath.com>
2025-07-31 18:32:14 +0300

    Remove comments from script

--- .ci/Scripts/getZlib.bat
+++ .ci/Scripts/getZlib.bat
@@ -11,15 +11,4 @@ git clone --no-checkout --filter=tree:0 --depth=1 --single-branch --branch=v1.3 
 git branch buildZlib v1.3
 git checkout buildZlib
 
-@REM rem build zlib
-@REM cmake -GNinja -B "./Build" ^
-@REM     -DCMAKE_BUILD_TYPE=Release ^
-@REM     -DCMAKE_SKIP_INSTALL_ALL_DEPENDENCY=ON ^
-@REM     -DCMAKE_INSTALL_PREFIX="./Install" ^
-@REM     -DLIBRESSL_APPS=OFF ^
-@REM     -DLIBRESSL_TESTS=OFF
-
-@REM cmake --build "./Build"
-@REM cmake --install "./Build"
-
 popd
